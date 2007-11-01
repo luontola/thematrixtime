493bd221fc8ff90d0e727009ce4535b1c0281951
Esko Luontola <esko.luontola@gmail.com>
2007-11-01 20:45:06 +0000

    Improved printing

--- src/main/java/net/orfjackal/thematrixtime/TheMatrixTime.java
+++ src/main/java/net/orfjackal/thematrixtime/TheMatrixTime.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
  */
 public class TheMatrixTime {
 
+    @SuppressWarnings({"InfiniteLoopStatement"})
     public static void main(String[] args) throws InterruptedException {
         Scanner in = new Scanner(System.in);
         System.out.println("The Matrix Time");
@@ -20,13 +21,13 @@ public class TheMatrixTime {
 
         double ratio = in.nextDouble();
         TimeCompressor compressor = new TimeCompressor(new AdjustableClock(new ReadOnlySystemClock()), ratio);
-        DateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+        DateFormat timeFormat = new SimpleDateFormat("HH:mm:ss.SSS");
 
+        System.out.println("SYSTEM TIME      REAL TIME");
         while (true) {
             Date systemTime = new Date(compressor.systemTime());
             Date realWorldTime = new Date(compressor.realWorldTime());
-            System.out.println("SYSTEM TIME      REAL TIME");
-            System.out.println(timeFormat.format(systemTime) + "         " + timeFormat.format(realWorldTime));
+            System.out.println(timeFormat.format(systemTime) + "     " + timeFormat.format(realWorldTime));
             for (int i = 0; i < 100; i++) {
                 Thread.sleep(10);
                 compressor.synchronizeClock();
